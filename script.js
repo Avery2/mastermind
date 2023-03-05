330fa477a4bdf3f3670ef38957b8a12be9c8b87a
Avery2 <justinaverychan@gmail.com>
2023-03-05 21:09:32 +0100

    feat: add tooltip

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ console.log("script loaded");
 
 const board = document.getElementById("board");
 
-function addChild() {
-  console.log("addChild");
+function addTableRow() {
+  console.log("addTableRow");
   const row = document.createElement("tr");
 
   for (let i = 0; i < 4; i++) {
@@ -16,3 +16,17 @@ function addChild() {
   }
   board.appendChild(row);
 }
+
+function populateDropdown(index) {
+  const dropdown = document.getElementById(`color-input-${index}`);
+  for (let i = 0; i < 4; i++) {
+    const newOption = document.createElement("option");
+    newOption.style = "background-color: red";
+    newOption.innerHTML = `${i}`;
+    dropdown.appendChild(newOption);
+  }
+}
+populateDropdown("1");
+populateDropdown("2");
+populateDropdown("3");
+populateDropdown("4");
